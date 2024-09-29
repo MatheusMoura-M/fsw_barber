8d2ca83fbcd6d337a042120f0fcb01c270e18ba3
Matheus <matheus150101miranda@gmail.com>
2024-09-29 09:45:04 -0300

    feat: add initial barbershop page

--- app/_components/barbershop-item.tsx
+++ app/_components/barbershop-item.tsx
@@ -14,7 +14,6 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
   return (
     <Card className="min-w-[167px] rounded-2xl">
       <CardContent className="px-1 pb-0 pt-1">
-        {/* IMAGEM */}
         <div className="relative h-[159px] w-full">
           <Image
             alt={barbershop.name}
@@ -32,10 +31,10 @@ const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
           </Badge>
         </div>
 
-        {/* TEXTO */}
         <div className="px-1 py-3">
           <h3 className="truncate font-semibold">{barbershop.name}</h3>
           <p className="truncate text-sm text-gray-400">{barbershop.address}</p>
+
           <Button variant="secondary" className="mt-3 w-full" asChild>
             <Link href={`/barbershops/${barbershop.id}`}>Reservar</Link>
           </Button>
--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,11 @@ import BarbershopItem from "./_components/barbershop-item"
 
 const Home = async () => {
   const barbershops = await db.barbershop.findMany({})
+  const popularBarbershops = await db.barbershop.findMany({
+    orderBy: {
+      name: "desc",
+    },
+  })
 
   return (
     <div>
@@ -75,9 +80,17 @@ const Home = async () => {
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
         </h2>
-
         <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Populares
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map((barbershop) => (
             <BarbershopItem key={barbershop.id} barbershop={barbershop} />
           ))}
         </div>
