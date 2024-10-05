006b8f98f6fddc7206bb77241d71836367b8fdf0
Matheus <matheus150101miranda@gmail.com>
2024-10-05 12:24:08 -0300

    feat: add sign in with google

--- app/_components/sign-in-dialog.tsx
+++ app/_components/sign-in-dialog.tsx
@@ -1,10 +1,10 @@
-// import { signIn } from "next-auth/react"
+import { signIn } from "next-auth/react"
 import { Button } from "./ui/button"
 import { DialogDescription, DialogHeader, DialogTitle } from "./ui/dialog"
 import Image from "next/image"
 
 const SignInDialog = () => {
-  //   const handleLoginWithGoogleClick = () => signIn("google")
+  const handleLoginWithGoogleClick = () => signIn("google")
 
   return (
     <>
@@ -18,7 +18,7 @@ const SignInDialog = () => {
       <Button
         variant="outline"
         className="gap-1 font-bold"
-        // onClick={handleLoginWithGoogleClick}
+        onClick={handleLoginWithGoogleClick}
       >
         <Image
           alt="Fazer login com o Google"
